c39b8cb97e91f44462179e0ffe15614932033882
StevenRS11 <stevenrs11@aol.com>
2013-02-26 11:29:17 -0500

    Fixed config option not changing properly Signed-off-by: StevenRS11 <stevenrs11@aol.com>

--- StevenDimDoors/mod_pocketDim/RiftGenerator.java
+++ StevenDimDoors/mod_pocketDim/RiftGenerator.java
@@ -139,7 +139,7 @@ public class RiftGenerator implements IWorldGenerator
     		}
     	
     	}
-    		if(random.nextInt(120)==0&&world.provider.getDimensionName()!="PocketDim"&&!world.isRemote)
+    		if(random.nextInt(120)==0&&world.provider.getDimensionName()!="PocketDim"&&!world.isRemote&&mod_pocketDim.riftsInWorldGen)
     		{
     		//	System.out.println("tryingToGen");
     			int blockID=Block.stoneBrick.blockID;
