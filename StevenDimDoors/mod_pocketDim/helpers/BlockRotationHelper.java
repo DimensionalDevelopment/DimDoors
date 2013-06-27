b1e18b564ee0c459434556245cfd152e1c3723e3
StevenRS11 <stevenrs11@aol.com>
2013-06-26 22:28:57 -0400

    Made it prettier

--- StevenDimDoors/mod_pocketDim/helpers/BlockRotationHelper.java
+++ StevenDimDoors/mod_pocketDim/helpers/BlockRotationHelper.java
@@ -13,8 +13,6 @@ public class BlockRotationHelper
 		this.InitializeRotationMap();
 	}
 	
-	
-	
 	public void InitializeRotationMap()
 	{
 		HashMap<Integer,HashMap<Integer, Integer>> orientation0 = new HashMap<Integer,HashMap<Integer, Integer>>();
@@ -29,29 +27,22 @@ public class BlockRotationHelper
 		stairs0.put(6, 5);
 		stairs0.put(5, 7);
 		stairs0.put(4, 6);
-		
-		
 
-		
-		HashMap chestsLadders0 = new HashMap();
+		HashMap<Integer,Integer> chestsLadders0 = new HashMap<Integer,Integer>();
 		
 		chestsLadders0.put(2, 5);
 		chestsLadders0.put(3, 4);
 		chestsLadders0.put(4, 2);
 		chestsLadders0.put(5, 3);
-		
-		
-		
-		HashMap vine0 = new HashMap();
+
+		HashMap<Integer,Integer> vine0 = new HashMap<Integer,Integer>();
 		
 		vine0.put(1, 2);
 		vine0.put(2, 4);
 		vine0.put(4, 8);
 		vine0.put(8, 1);
 		
-		orientation0.put(Block.vine.blockID, vine0);
-
-		HashMap leverButtonTorch0 = new HashMap();
+		HashMap<Integer,Integer> leverButtonTorch0 = new HashMap<Integer,Integer>();
 		
 		leverButtonTorch0.put(12, 9);
 		leverButtonTorch0.put(11, 10);
@@ -62,9 +53,7 @@ public class BlockRotationHelper
 		leverButtonTorch0.put(1, 3);
 		leverButtonTorch0.put(4, 1);
 		
-		
-		
-		HashMap pistonDropperDispenser0 = new HashMap();
+		HashMap<Integer,Integer> pistonDropperDispenser0 = new HashMap<Integer,Integer>();
 		
 		pistonDropperDispenser0.put(4, 2);
 		pistonDropperDispenser0.put(5, 3);
@@ -74,11 +63,8 @@ public class BlockRotationHelper
 		pistonDropperDispenser0.put(11, 12);
 		pistonDropperDispenser0.put(10, 13);
 		pistonDropperDispenser0.put(12, 10);
-		
-		
-		
-		
-		HashMap repeaterComparatorDoorTripwire0 = new HashMap();
+
+		HashMap<Integer,Integer> repeaterComparatorDoorTripwire0 = new HashMap<Integer,Integer>();
 		
 		repeaterComparatorDoorTripwire0.put(0, 1);
 		repeaterComparatorDoorTripwire0.put(1, 2);
@@ -97,11 +83,9 @@ public class BlockRotationHelper
 		repeaterComparatorDoorTripwire0.put(14, 15);
 		repeaterComparatorDoorTripwire0.put(15, 12);
 		
-
-		
 		HashMap<Integer,HashMap<Integer, Integer>> orientation1 = new HashMap<Integer,HashMap<Integer, Integer>>();
 		
-		HashMap stairs1 = new HashMap();
+		HashMap<Integer,Integer> stairs1 = new HashMap<Integer,Integer>();
 		
 		stairs1.put(0, 1);
 		stairs1.put(1, 0);
@@ -112,21 +96,21 @@ public class BlockRotationHelper
 		stairs1.put(5, 4);
 		stairs1.put(4, 5);
 		
-		HashMap chestsLadders1 = new HashMap();
+		HashMap<Integer,Integer> chestsLadders1 = new HashMap<Integer,Integer>();
 		
 		chestsLadders1.put(2, 3);
 		chestsLadders1.put(3, 2);
 		chestsLadders1.put(4, 5);
 		chestsLadders1.put(5, 4);
 		
-		HashMap vine1 = new HashMap();
+		HashMap<Integer,Integer> vine1 = new HashMap<Integer,Integer>();
 		
 		vine1.put(1, 4);
 		vine1.put(2, 8);
 		vine1.put(4, 1);
 		vine1.put(8, 2);
 		
-		HashMap leverButtonTorch1 = new HashMap();
+		HashMap<Integer,Integer> leverButtonTorch1 = new HashMap<Integer,Integer>();
 		
 		leverButtonTorch1.put(12, 9);
 		leverButtonTorch1.put(11, 10);
@@ -137,7 +121,7 @@ public class BlockRotationHelper
 		leverButtonTorch1.put(1, 3);
 		leverButtonTorch1.put(4, 1);
 		
-		HashMap pistonDropperDispenser1 = new HashMap();
+		HashMap<Integer,Integer> pistonDropperDispenser1 = new HashMap<Integer,Integer>();
 		
 		pistonDropperDispenser1.put(12, 11);
 		pistonDropperDispenser1.put(11, 12);
@@ -149,7 +133,7 @@ public class BlockRotationHelper
 		pistonDropperDispenser1.put(4,3);
 		
 		
-		HashMap repeaterComparatorDoorTripwire1 = new HashMap();
+		HashMap<Integer,Integer> repeaterComparatorDoorTripwire1 = new HashMap<Integer,Integer>();
 		
 		repeaterComparatorDoorTripwire1.put(0, 2);
 		repeaterComparatorDoorTripwire1.put(1, 3);
@@ -170,7 +154,7 @@ public class BlockRotationHelper
 		
 		HashMap<Integer,HashMap<Integer, Integer>> orientation2 = new HashMap<Integer,HashMap<Integer, Integer>>();
 		
-		HashMap stairs2 = new HashMap();
+		HashMap<Integer,Integer> stairs2 = new HashMap<Integer,Integer>();
 		
 		stairs2.put(2, 0);
 		stairs2.put(3, 1);
@@ -181,21 +165,21 @@ public class BlockRotationHelper
 		stairs2.put(7, 5);
 		stairs2.put(6, 4);
 		
-		HashMap chestsLadders2 = new HashMap();
+		HashMap<Integer,Integer> chestsLadders2 = new HashMap<Integer,Integer>();
 		
 		chestsLadders2.put(2, 4);
 		chestsLadders2.put(3, 5);
 		chestsLadders2.put(4, 3);
 		chestsLadders2.put(5, 2);
 		
-		HashMap vine2 = new HashMap();
+		HashMap<Integer,Integer> vine2 = new HashMap<Integer,Integer>();
 		
 		vine2.put(1, 8);
 		vine2.put(2, 1);
 		vine2.put(4, 2);
 		vine2.put(8, 4);
 		
-		HashMap leverButtonTorch2 = new HashMap();
+		HashMap<Integer,Integer> leverButtonTorch2 = new HashMap<Integer,Integer>();
 		
 		leverButtonTorch2.put(9, 12);
 		leverButtonTorch2.put(10, 11);
@@ -206,7 +190,7 @@ public class BlockRotationHelper
 		leverButtonTorch2.put(3, 1);
 		leverButtonTorch2.put(1, 4);
 		
-		HashMap pistonDropperDispenser2 = new HashMap();
+		HashMap<Integer,Integer> pistonDropperDispenser2 = new HashMap<Integer,Integer>();
 		
 		pistonDropperDispenser2.put(2, 4);
 		pistonDropperDispenser2.put(3, 5);
@@ -218,7 +202,7 @@ public class BlockRotationHelper
 		pistonDropperDispenser2.put(13,10);
 		
 		
-		HashMap repeaterComparatorDoorTripwire2 = new HashMap();
+		HashMap<Integer,Integer> repeaterComparatorDoorTripwire2 = new HashMap<Integer,Integer>();
 		
 		repeaterComparatorDoorTripwire2.put(1, 0);
 		repeaterComparatorDoorTripwire2.put(2, 1);
@@ -251,25 +235,22 @@ public class BlockRotationHelper
 		orientation0.put(Block.stairsWoodOak.blockID, stairs0);
 		orientation0.put(Block.stairsWoodSpruce.blockID, stairs0);
 		orientation0.put(Block.stairsBrick.blockID, stairs0);
-		
+		orientation0.put(Block.vine.blockID, vine0);
 		orientation0.put(Block.chest.blockID, chestsLadders0);
 		orientation0.put(Block.chestTrapped.blockID, chestsLadders0);
 		orientation0.put(Block.ladder.blockID, chestsLadders0);
-		
 		orientation0.put(Block.lever.blockID, leverButtonTorch0);
 		orientation0.put(Block.stoneButton.blockID, leverButtonTorch0);
 		orientation0.put(Block.woodenButton.blockID, leverButtonTorch0);
 		orientation0.put(Block.torchRedstoneActive.blockID, leverButtonTorch0);
 		orientation0.put(Block.torchRedstoneIdle.blockID, leverButtonTorch0);
 		orientation0.put(Block.torchWood.blockID, leverButtonTorch0);
-		
 		orientation0.put(Block.pistonBase.blockID,pistonDropperDispenser0);
 		orientation0.put(Block.pistonExtension.blockID,pistonDropperDispenser0);
 		orientation0.put(Block.pistonMoving.blockID,pistonDropperDispenser0);
 		orientation0.put(Block.pistonStickyBase.blockID,pistonDropperDispenser0);
 		orientation0.put(Block.dropper.blockID,pistonDropperDispenser0);
 		orientation0.put(Block.dispenser.blockID,pistonDropperDispenser0);
-		
 		orientation0.put(Block.redstoneComparatorActive.blockID,pistonDropperDispenser0);
 		orientation0.put(Block.redstoneComparatorIdle.blockID,pistonDropperDispenser0);
 		orientation0.put(Block.redstoneRepeaterActive.blockID,pistonDropperDispenser0);
@@ -277,10 +258,6 @@ public class BlockRotationHelper
 		orientation0.put(Block.doorWood.blockID,pistonDropperDispenser0);
 		orientation0.put(Block.doorIron.blockID,pistonDropperDispenser0);
 		orientation0.put(Block.tripWireSource.blockID,pistonDropperDispenser0);
-		
-		
-		
-		
 		
 		orientation1.put(Block.stairsBrick.blockID, stairs1);
 		orientation1.put(Block.stairsCobblestone.blockID, stairs1);
@@ -293,25 +270,22 @@ public class BlockRotationHelper
 		orientation1.put(Block.stairsWoodOak.blockID, stairs1);
 		orientation1.put(Block.stairsWoodSpruce.blockID, stairs1);
 		orientation1.put(Block.stairsBrick.blockID, stairs1);
-		
+		orientation1.put(Block.vine.blockID, vine1);
 		orientation1.put(Block.chest.blockID, chestsLadders1);
 		orientation1.put(Block.chestTrapped.blockID, chestsLadders1);
 		orientation1.put(Block.ladder.blockID, chestsLadders1);
-		
 		orientation1.put(Block.lever.blockID, leverButtonTorch1);
 		orientation1.put(Block.stoneButton.blockID, leverButtonTorch1);
 		orientation1.put(Block.woodenButton.blockID, leverButtonTorch1);
 		orientation1.put(Block.torchRedstoneActive.blockID, leverButtonTorch1);
 		orientation1.put(Block.torchRedstoneIdle.blockID, leverButtonTorch1);
 		orientation1.put(Block.torchWood.blockID, leverButtonTorch1);
-		
 		orientation1.put(Block.pistonBase.blockID,pistonDropperDispenser1);
 		orientation1.put(Block.pistonExtension.blockID,pistonDropperDispenser1);
 		orientation1.put(Block.pistonMoving.blockID,pistonDropperDispenser1);
 		orientation1.put(Block.pistonStickyBase.blockID,pistonDropperDispenser1);
 		orientation1.put(Block.dropper.blockID,pistonDropperDispenser1);
 		orientation1.put(Block.dispenser.blockID,pistonDropperDispenser1);
-		
 		orientation1.put(Block.redstoneComparatorActive.blockID,pistonDropperDispenser1);
 		orientation1.put(Block.redstoneComparatorIdle.blockID,pistonDropperDispenser1);
 		orientation1.put(Block.redstoneRepeaterActive.blockID,pistonDropperDispenser1);
@@ -320,11 +294,7 @@ public class BlockRotationHelper
 		orientation1.put(Block.doorIron.blockID,pistonDropperDispenser1);
 		orientation1.put(Block.tripWireSource.blockID,pistonDropperDispenser1);
 		
-		
-		
-		
-		
-		
+
 		orientation2.put(Block.stairsBrick.blockID, stairs2);
 		orientation2.put(Block.stairsCobblestone.blockID, stairs2);
 		orientation2.put(Block.stairsNetherBrick.blockID, stairs2);
@@ -336,25 +306,22 @@ public class BlockRotationHelper
 		orientation2.put(Block.stairsWoodOak.blockID, stairs2);
 		orientation2.put(Block.stairsWoodSpruce.blockID, stairs2);
 		orientation2.put(Block.stairsBrick.blockID, stairs2);
-		
+		orientation2.put(Block.vine.blockID, vine2);
 		orientation2.put(Block.chest.blockID, chestsLadders2);
 		orientation2.put(Block.chestTrapped.blockID, chestsLadders2);
 		orientation2.put(Block.ladder.blockID, chestsLadders2);
-		
 		orientation2.put(Block.lever.blockID, leverButtonTorch2);
 		orientation2.put(Block.stoneButton.blockID, leverButtonTorch2);
 		orientation2.put(Block.woodenButton.blockID, leverButtonTorch2);
 		orientation2.put(Block.torchRedstoneActive.blockID, leverButtonTorch2);
 		orientation2.put(Block.torchRedstoneIdle.blockID, leverButtonTorch2);
 		orientation2.put(Block.torchWood.blockID, leverButtonTorch2);
-		
 		orientation2.put(Block.pistonBase.blockID,pistonDropperDispenser2);
 		orientation2.put(Block.pistonExtension.blockID,pistonDropperDispenser2);
 		orientation2.put(Block.pistonMoving.blockID,pistonDropperDispenser2);
 		orientation2.put(Block.pistonStickyBase.blockID,pistonDropperDispenser2);
 		orientation2.put(Block.dropper.blockID,pistonDropperDispenser2);
 		orientation2.put(Block.dispenser.blockID,pistonDropperDispenser2);
-		
 		orientation2.put(Block.redstoneComparatorActive.blockID,pistonDropperDispenser2);
 		orientation2.put(Block.redstoneComparatorIdle.blockID,pistonDropperDispenser2);
 		orientation2.put(Block.redstoneRepeaterActive.blockID,pistonDropperDispenser2);
@@ -366,8 +333,6 @@ public class BlockRotationHelper
 		this.rotationMappings.put(2, orientation2);
 		this.rotationMappings.put(1, orientation1);
 		this.rotationMappings.put(0, orientation0);
-		
-		
 	}
 	
 	public int getRotatedBlock(int metaData, int desiredOrientation, int blockID)
@@ -382,8 +347,7 @@ public class BlockRotationHelper
 				}
 			}
 		}
-		
-		return 0 ;
+		return 0 ;	
 	}
 	
 	
