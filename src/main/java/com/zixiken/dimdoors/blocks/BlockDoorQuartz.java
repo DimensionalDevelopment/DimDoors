989c91751532dbdf45cd31f15c7c4bb4b9d55d9c
zangamj <darknessandshadows@gmail.com>
2016-07-12 14:03:40 -0400

    Finished BlockDoorQuartz

--- src/main/java/com/zixiken/dimdoors/blocks/BlockDoorQuartz.java
+++ src/main/java/com/zixiken/dimdoors/blocks/BlockDoorQuartz.java
@@ -3,12 +3,14 @@ package com.zixiken.dimdoors.blocks;
 import java.util.Random;
 
 import com.zixiken.dimdoors.DimDoors;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.BlockDoor;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
+import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockDoorQuartz extends BlockDoor {
     public static final String ID = "doorQuartz";
@@ -18,22 +20,13 @@ public class BlockDoorQuartz extends BlockDoor {
         setHardness(0.1F);
         setUnlocalizedName(ID);
 	}
-	
-	@SideOnly(Side.CLIENT)
-    protected String getTextureName()
-    {
-        return DimDoors.modid + ":" + this.getUnlocalizedName();
-    }
-	
-	@Override
-	public Item getItemDropped(int par1, Random par2Random, int par3)
-    {
-        return (par1 & 8) != 0 ? null : DimDoors.itemQuartzDoor;
+
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+        return state.getValue(BlockDoor.HALF) == EnumDoorHalf.LOWER ? DimDoors.itemQuartzDoor : null;
     }
 
     @Override
     @SideOnly(Side.CLIENT)
-    public Item getItem(World world, int x, int y, int z) {
-        return DimDoors.itemQuartzDoor;
-    }
+    public Item getItem(World worldIn, BlockPos pos) {return DimDoors.itemQuartzDoor;}
 }
