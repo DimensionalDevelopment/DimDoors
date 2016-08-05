1ee1583a06100f0946e3ef35140acb3936be4ace
zangamj <darknessandshadows@gmail.com>
2016-08-05 10:21:38 -0400

    Finished CloudRenderBlank.

--- src/main/java/com/zixiken/dimdoors/CloudRenderBlank.java
+++ src/main/java/com/zixiken/dimdoors/CloudRenderBlank.java
@@ -3,15 +3,11 @@ package com.zixiken.dimdoors;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraftforge.client.IRenderHandler;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class CloudRenderBlank extends IRenderHandler
-{
+public class CloudRenderBlank extends IRenderHandler {
     @Override
 	@SideOnly(Side.CLIENT)
-    public void render(float partialTicks, WorldClient world, Minecraft mc)
-    {
-    	
-    }
+    public void render(float partialTicks, WorldClient world, Minecraft mc) {}
 }
