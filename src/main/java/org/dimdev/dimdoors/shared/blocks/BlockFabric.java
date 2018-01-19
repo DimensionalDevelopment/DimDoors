5aaa6713284c246eb5ad235f535cda339fcafacb
Robijnvogel <robijn.bird@gmail.com>
2018-01-19 13:21:04 +0100

    Colourful Fabric
    -Added textures of Coloured Fabric Blocks
    -Enabled all the Coloured Fabric (fullbright colour) blocks
    -Added English lang names for Coloured Fabric

--- src/main/java/org/dimdev/dimdoors/shared/blocks/BlockFabric.java
+++ src/main/java/org/dimdev/dimdoors/shared/blocks/BlockFabric.java
@@ -6,7 +6,6 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.EnumDyeColor;
@@ -14,7 +13,6 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -36,12 +34,6 @@ public class BlockFabric extends BlockColored {
         setHardness(0.1F);
         setSoundType(SoundType.STONE);
         setLightLevel(1);
-    }
-
-    @Override
-    public void getSubBlocks(CreativeTabs item, NonNullList<ItemStack> items) { // TODO: make textures for all colors
-        items.add(new ItemStack(this, 1, EnumDyeColor.BLACK.getMetadata()));
-        items.add(new ItemStack(this, 1, EnumDyeColor.WHITE.getMetadata()));
     }
 
     @Override
