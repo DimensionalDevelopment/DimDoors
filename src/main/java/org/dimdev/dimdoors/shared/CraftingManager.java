24c02fba5df0411a1f65eeadfcbc08d6352b5eec
Robijnvogel <robijn.bird@gmail.com>
2018-02-28 18:46:52 +0100

    One x too little
    - Repaired a small mistake I made.

--- src/main/java/org/dimdev/dimdoors/shared/CraftingManager.java
+++ src/main/java/org/dimdev/dimdoors/shared/CraftingManager.java
@@ -57,7 +57,7 @@ public final class CraftingManager {
                 "yxy", 'x', Items.ENDER_PEARL, 'y', Items.OAK_DOOR));
 
         event.getRegistry().register(makeShapedRecipe(new ItemStack(ModBlocks.WOOD_DIMENSIONAL_TRAPDOOR, 1),
-                "yx", Items.ENDER_PEARL, 'y', Blocks.TRAPDOOR));
+                "yx", 'x', Items.ENDER_PEARL, 'y', Blocks.TRAPDOOR));
 
         event.getRegistry().register(makeShapedRecipe(new ItemStack(ModItems.GOLD_DIMENSIONAL_DOOR, 1),
                 "yxy", 'x', ModItems.STABLE_FABRIC, 'y', ModItems.GOLD_DOOR));
