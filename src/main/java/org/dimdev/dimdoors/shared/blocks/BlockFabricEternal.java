419593ae207dba9679ff7405a645c9ba21cecc2b
Mysticpasta1 <45608372+Mysticpasta1@users.noreply.github.com>
2020-05-01 19:45:12 -0500

    fixed invalid player crash with eternal fabric (fluid)
    a nether portal will spawn with you in the overworld now!

--- src/main/java/org/dimdev/dimdoors/shared/blocks/BlockFabricEternal.java
+++ src/main/java/org/dimdev/dimdoors/shared/blocks/BlockFabricEternal.java
@@ -1,24 +1,39 @@
 package org.dimdev.dimdoors.shared.blocks;
 
+import io.github.opencubicchunks.cubicchunks.core.world.ICubeProviderInternal;
 import net.minecraft.block.BlockEmptyDrops;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.DimensionType;
 import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeChunkManager;
+import net.minecraftforge.event.terraingen.InitNoiseGensEvent;
 import net.minecraftforge.fluids.BlockFluidClassic;
+import org.apache.logging.log4j.core.jmx.Server;
+import org.dimdev.ddutils.TeleportUtils;
 import org.dimdev.dimdoors.DimDoors;
 import org.dimdev.dimdoors.shared.fluids.ModFluids;
 import org.dimdev.dimdoors.shared.items.ModCreativeTabs;
 import org.dimdev.dimdoors.shared.rifts.targets.EscapeTarget;
+import org.dimdev.dimdoors.shared.rifts.targets.ITarget;
+import org.lwjgl.util.Dimension;
 
 public class BlockFabricEternal extends BlockFluidClassic { // TODO: make this a glowing red liquid
 
     public static final Material ETERNAL_FABRIC = new Material(MapColor.PINK);
     public static final String ID = "eternal_fabric";
     public static EscapeTarget exitLimbo = new EscapeTarget(true);
+
+
 
     public BlockFabricEternal() {
         super(ModFluids.ETERNAL_FABRIC, ETERNAL_FABRIC);
@@ -33,9 +48,14 @@ public class BlockFabricEternal extends BlockFluidClassic { // TODO: make this a
     }
 
     //This is meant to be a fix for a cubic chunks version. Currently crashes for some reason and don't want to try and fix right now.
-    /*@Override
+
+    @Override
     public void onEntityWalk(World world, BlockPos pos, Entity entity) {
+        MinecraftServer minecraftServer = world.getMinecraftServer();
         if (world.isRemote) return;
-        exitLimbo.receiveEntity(entity, entity.rotationYaw / 90 * 90, 0);
-    }*/
+        entity.setPortal(entity.getPosition());
+        entity.changeDimension(-1);
+    }
 }
+
+
