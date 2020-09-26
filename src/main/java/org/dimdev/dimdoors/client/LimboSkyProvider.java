0b0c4d5d1682ae0081edf971af33f2d0290e25a3
SD <harrypotter.starwars.shrish@gmail.com>
2020-09-26 13:08:17 +0530

    Fixed the limbo sky color
     Changes to be committed:
    	modified:   src/main/java/org/dimdev/dimdoors/client/LimboSkyProvider.java

--- src/main/java/org/dimdev/dimdoors/client/LimboSkyProvider.java
+++ src/main/java/org/dimdev/dimdoors/client/LimboSkyProvider.java
@@ -9,6 +9,6 @@ public class LimboSkyProvider extends CustomSkyProvider {
     private static final Identifier SUN_RENDER_PATH = new Identifier("dimdoors:textures/other/limbo_sun.png");
 
     public LimboSkyProvider() {
-        super(MOON_RENDER_PATH, SUN_RENDER_PATH, new Vec3i(255, 255, 255));
+        super(MOON_RENDER_PATH, SUN_RENDER_PATH, new Vec3i(25, 25, 25));
     }
 }
