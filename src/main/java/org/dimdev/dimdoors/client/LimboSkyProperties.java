2a5e366aa35e74aad86cc0ee4a8d68294435820d
SD <harrypotter.starwars.shrish@gmail.com>
2020-09-27 12:24:18 +0530

    Re added sky properties
     Changes to be committed:
    	new file:   src/main/java/org/dimdev/dimdoors/client/LimboSkyProperties.java
    	modified:   src/min/java/org/dimdev/dimdoors/client/ModRendering.java
    	modified:   src/main/java/org/dimdev/dimdoors/world/ModBiomes.java

---
+++ src/main/java/org/dimdev/dimdoors/client/LimboSkyProperties.java
@@ -0,0 +1,20 @@
+package org.dimdev.dimdoors.client;
+
+import net.minecraft.client.render.SkyProperties;
+import net.minecraft.util.math.Vec3d;
+
+public class LimboSkyProperties extends SkyProperties {
+    public LimboSkyProperties() {
+        super(Float.NaN, true, SkyType.NORMAL, true, true);
+    }
+
+    @Override
+    public Vec3d adjustSkyColor(Vec3d color, float sunHeight) {
+        return color;
+    }
+
+    @Override
+    public boolean useThickFog(int camX, int camY) {
+        return true;
+    }
+}
