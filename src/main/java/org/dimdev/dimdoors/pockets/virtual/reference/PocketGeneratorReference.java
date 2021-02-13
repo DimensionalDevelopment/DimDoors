2a07a0db7766ddcfefe4e3d80aef0e470fa2cd51
CreepyCre <CreepyCre@gmail.com>
2021-02-13 14:05:25 +0100

    added "setup_loot" boolean to PocketGenerator

--- src/main/java/org/dimdev/dimdoors/pockets/virtual/reference/PocketGeneratorReference.java
+++ src/main/java/org/dimdev/dimdoors/pockets/virtual/reference/PocketGeneratorReference.java
@@ -100,7 +100,7 @@ public abstract class PocketGeneratorReference extends VirtualSingularPocket {
 		generator.applyModifiers(parameters, manager);
 
 		this.applyModifiers(parameters, manager);
-		generator.setup(pocket, parameters, setupLoot != null ? setupLoot : true);
+		generator.setup(pocket, parameters, setupLoot != null ? setupLoot : (generator.getSetupLoot()) != null ? generator.getSetupLoot() : true);
 		return pocket;
 	}
 
