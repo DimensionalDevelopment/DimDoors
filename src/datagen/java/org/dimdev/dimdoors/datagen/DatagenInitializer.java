7f68a3ecb8c1ad8fee4d43677c34ba9b28f6c6cc
SD <harrypotter.starwars.shrish@gmail.com>
2021-02-17 04:03:11 +0100

    Working data generator

--- src/datagen/java/org/dimdev/dimdoors/datagen/DatagenInitializer.java
+++ src/datagen/java/org/dimdev/dimdoors/datagen/DatagenInitializer.java
@@ -1,24 +1,37 @@
 package org.dimdev.dimdoors.datagen;
 
+import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.Map;
+import java.util.Collections;
 
-import me.shedaniel.cloth.api.datagen.v1.DataGeneratorHandler;
-import me.shedaniel.cloth.api.datagen.v1.RecipeData;
 import org.dimdev.dimdoors.block.ModBlocks;
 import org.dimdev.dimdoors.item.ModItems;
 
-import net.minecraft.block.Block;
-import net.minecraft.util.DyeColor;
+import net.minecraft.Bootstrap;
+import net.minecraft.data.DataGenerator;
 
-public class DatagenInitializer {
-	public static void main(String[] args) {
-		ModBlocks.init();
-		ModItems.init();
-		DataGeneratorHandler handler = DataGeneratorHandler.create(Paths.get("./generated"));
-		RecipeData recipes = handler.getRecipes();
-		for (Map.Entry<DyeColor, Block> entry : ModBlocks.FABRIC_BLOCKS.entrySet()) {
-			// TODO: add recipes for fabric
+import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
+
+public class DatagenInitializer implements PreLaunchEntrypoint {
+	public static RecipeConsumer RECIPE_CONSUMER;
+
+	// How to run the data generator:-
+	// - Duplicate the Minecraft Client run config
+	// - Change the module from DimensionalDoors.main to DimensionalDoors.datagen
+	// - Profit
+	@Override
+	public void onPreLaunch() {
+		try {
+			Bootstrap.initialize();
+			ModBlocks.init();
+			ModItems.init();
+			DataGenerator dataGenerator = new DataGenerator(Paths.get("./generated"), Collections.emptyList());
+			dataGenerator.install(new FabricRecipeHandler(dataGenerator));
+			dataGenerator.install(RECIPE_CONSUMER = new RecipeConsumer(dataGenerator));
+			dataGenerator.run();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
 		}
+		System.exit(0);
 	}
 }
