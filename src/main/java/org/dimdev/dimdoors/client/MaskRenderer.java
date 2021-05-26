b3c23a1ccaf7687a1c08a1d7763bcca25728a0cc
Waterpicker <Waterpickerenternity@gmail.com>
2021-05-26 02:09:56 -0500

    Houston we have a mask!

--- src/main/java/org/dimdev/dimdoors/client/MaskRenderer.java
+++ src/main/java/org/dimdev/dimdoors/client/MaskRenderer.java
@@ -1,24 +1,17 @@
 package org.dimdev.dimdoors.client;
 
+import net.minecraft.client.render.entity.EntityRendererFactory;
+
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import org.dimdev.dimdoors.entity.MaskEntity;
-
-import net.minecraft.client.render.entity.EntityRenderDispatcher;
-import net.minecraft.client.render.entity.EntityRenderer;
-import net.minecraft.client.render.entity.EntityRendererFactory;
-import net.minecraft.util.Identifier;
-
-import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
+import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.renderer.geo.GeoEntityRenderer;
 
 @Environment(EnvType.CLIENT)
-public class MaskRenderer extends EntityRenderer<MaskEntity> {
-	public MaskRenderer(EntityRendererFactory.Context ctx) {
-        super(ctx);
-    }
-
-    @Override
-    public Identifier getTexture(MaskEntity entity) {
-        return new Identifier("dimdoors:mask");
+public class MaskRenderer extends GeoEntityRenderer<MaskEntity> {
+    public MaskRenderer(EntityRendererFactory.Context ctx) {
+        super(ctx, new MaskModel());
+        this.shadowRadius = 0.7f;
     }
 }
