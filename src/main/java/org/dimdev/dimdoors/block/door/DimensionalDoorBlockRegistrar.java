108ef37bf3c14ebf383d0b3ee7c8820058d94dfe
CreepyCre <CreepyCre@gmail.com>
2021-09-10 15:30:26 +0200

    improvements to autogen door model provider fixes #235

--- src/main/java/org/dimdev/dimdoors/block/door/DimensionalDoorBlockRegistrar.java
+++ src/main/java/org/dimdev/dimdoors/block/door/DimensionalDoorBlockRegistrar.java
@@ -7,23 +7,23 @@ import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.DoorBlock;
-import net.minecraft.block.TrapdoorBlock;
+import net.minecraft.block.*;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.Property;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 import org.dimdev.dimdoors.DimensionalDoorsInitializer;
-import org.dimdev.dimdoors.block.door.data.DoorData;
 import org.dimdev.dimdoors.block.entity.ModBlockEntityTypes;
 import org.dimdev.dimdoors.item.DimensionalDoorItemRegistrar;
 import org.dimdev.dimdoors.listener.BlockRegistryEntryAddedListener;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.function.BiFunction;
 
 public class DimensionalDoorBlockRegistrar {
@@ -50,9 +50,9 @@ public class DimensionalDoorBlockRegistrar {
 	public void handleEntry(Identifier identifier, Block original) {
 		if (DimensionalDoorsInitializer.getConfig().getDoorsConfig().isAllowed(identifier)) {
 			if (!(original instanceof DimensionalDoorBlock) && original instanceof DoorBlock) {
-				register(identifier, original, AutoGenDimensionalDoorBlock::new);
+				register(identifier, original, DimensionalDoorBlockRegistrar::createAutoGenDimensionalDoorBlock);
 			} else if (!(original instanceof DimensionalTrapdoorBlock) && original instanceof TrapdoorBlock) {
-				register(identifier, original, AutoGenDimensionalTrapdoorBlock::new);
+				register(identifier, original, DimensionalDoorBlockRegistrar::createAutoGenDimensionalTrapdoorBlock);
 			}
 		}
 	}
@@ -81,12 +81,54 @@ public class DimensionalDoorBlockRegistrar {
 		return mappedDoorBlocks.containsKey(identifier);
 	}
 
+	private static <T extends Comparable<T>> BlockState transferProperty(BlockState from, BlockState to, Property<T> property) {
+		return to.with(property, from.get(property));
+	}
+
+	private static AutoGenDimensionalDoorBlock createAutoGenDimensionalDoorBlock(AbstractBlock.Settings settings, Block originalBlock) {
+		return new AutoGenDimensionalDoorBlock(settings, originalBlock) {
+			@Override
+			protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+				appendPropertiesOverride(builder, originalBlock, WATERLOGGED);
+			}
+		};
+	}
+
+	private static AutoGenDimensionalTrapdoorBlock createAutoGenDimensionalTrapdoorBlock(AbstractBlock.Settings settings, Block originalBlock) {
+		return new AutoGenDimensionalTrapdoorBlock(settings, originalBlock) {
+			@Override
+			protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+				appendPropertiesOverride(builder, originalBlock, WATERLOGGED);
+			}
+		};
+	}
+
+	private static void appendPropertiesOverride(StateManager.Builder<Block, BlockState> builder, Block originalBlock, Property<?>... requiredProperties) {
+		HashSet<Property<?>> properties = new HashSet<>(originalBlock.getStateManager().getProperties());
+		properties.addAll(List.of(requiredProperties));
+		builder.add(properties.toArray(new Property[0]));
+	}
+
 	private static class AutoGenDimensionalDoorBlock extends DimensionalDoorBlock {
 		private final Block originalBlock;
 
 		public AutoGenDimensionalDoorBlock(Settings settings, Block originalBlock) {
 			super(settings);
 			this.originalBlock = originalBlock;
+
+			BlockState state = this.getStateManager().getDefaultState();
+			BlockState originalState = originalBlock.getDefaultState();
+			for (Property<?> property : originalBlock.getDefaultState().getProperties()) {
+				state = transferProperty(originalState, state, property);
+			}
+			setDefaultState(state.with(WATERLOGGED, false));
+		}
+
+		@Override
+		protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+			// This method has to be defined in an anonymous inner class,
+			// 		since Block#appendProperties is run before originalBlock can be set.
+			throw new RuntimeException("AutoGenDimensionalDoorBlock should be instantiated as anonymous inner class overriding appendProperties!");
 		}
 
 		@Override
@@ -101,6 +143,20 @@ public class DimensionalDoorBlockRegistrar {
 		public AutoGenDimensionalTrapdoorBlock(Settings settings, Block originalBlock) {
 			super(settings);
 			this.originalBlock = originalBlock;
+
+			BlockState state = this.getStateManager().getDefaultState();
+			BlockState originalState = originalBlock.getDefaultState();
+			for (Property<?> property : originalBlock.getDefaultState().getProperties()) {
+				state = transferProperty(originalState, state, property);
+			}
+			setDefaultState(state.with(WATERLOGGED, false));
+		}
+
+		@Override
+		protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+			// This method has to be defined in an anonymous inner class,
+			// 		since Block#appendProperties is run before originalBlock can be set.
+			throw new RuntimeException("AutoGenDimensionalTrapdoorBlock should be instantiated as anonymous inner class overriding appendProperties!");
 		}
 
 		@Override
--- src/main/java/org/dimdev/dimdoors/client/DimensionalDoorModelVariantProvider.java
+++ src/main/java/org/dimdev/dimdoors/client/DimensionalDoorModelVariantProvider.java
@@ -3,15 +3,20 @@ package org.dimdev.dimdoors.client;
 import net.fabricmc.fabric.api.client.model.ModelProviderContext;
 import net.fabricmc.fabric.api.client.model.ModelProviderException;
 import net.fabricmc.fabric.api.client.model.ModelVariantProvider;
+import net.minecraft.block.Block;
 import net.minecraft.client.render.model.UnbakedModel;
 import net.minecraft.client.util.ModelIdentifier;
+import net.minecraft.state.property.Property;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.dimdev.dimdoors.DimensionalDoorsInitializer;
 import org.dimdev.dimdoors.block.door.DimensionalDoorBlockRegistrar;
 import org.dimdev.dimdoors.item.DimensionalDoorItemRegistrar;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class DimensionalDoorModelVariantProvider implements ModelVariantProvider {
 	private static final Identifier childItem = new Identifier("dimdoors:item/child_item");
@@ -23,15 +28,19 @@ public class DimensionalDoorModelVariantProvider implements ModelVariantProvider
 		DimensionalDoorBlockRegistrar blockRegistrar = DimensionalDoorsInitializer.getDimensionalDoorBlockRegistrar();
 		if (blockRegistrar.isMapped(identifier)) {
 			Identifier mapped = blockRegistrar.get(identifier);
-			ModelIdentifier newId = new ModelIdentifier(mapped, modelId.getVariant());
-			UnbakedModel model = context.loadModel(newId);
-			if (model != null) return model;
+			//ModelIdentifier newId = new ModelIdentifier(mapped, modelId.getVariant());
+			//UnbakedModel model = context.loadModel(newId);
+			//if (model != null) return model;
+
+			Block original = Registry.BLOCK.get(mapped);
+			Set<String> originalProperties = original.getStateManager().getProperties().stream().map(Property::getName).collect(Collectors.toSet());
+
 			ArrayList<String> variantArray = new ArrayList<>();
 			for (String part : modelId.getVariant().split(",")) {
-				if (!part.startsWith("waterlogged")) variantArray.add(part);
+				if (originalProperties.contains(part.split("=")[0])) variantArray.add(part);
 			}
 			String variant = String.join(",", variantArray);
-			newId = new ModelIdentifier(mapped, variant);
+			ModelIdentifier newId = new ModelIdentifier(mapped, variant);
 			return context.loadModel(newId);
 		} else if (identifier.getPath().startsWith(DimensionalDoorItemRegistrar.PREFIX)) {
 			return context.loadModel(childItem);
