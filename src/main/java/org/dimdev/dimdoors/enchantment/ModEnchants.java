b5ae14b81c10181b472a72cac2e61bc6bf4f8531
SD <shrish.deshpande@icloud.com>
2022-01-14 13:29:55 +0530

    forgot to update fapi smh

--- src/main/java/org/dimdev/dimdoors/enchantment/ModEnchants.java
+++ src/main/java/org/dimdev/dimdoors/enchantment/ModEnchants.java
@@ -7,14 +7,13 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModEnchants {
-	//public static Enchantment STRING_THEORY_ENCHANTMENT;
+	public static Enchantment STRING_THEORY_ENCHANTMENT;
+
 	public static void init() {
-		/*
 		STRING_THEORY_ENCHANTMENT = Registry.register(
 				Registry.ENCHANTMENT,
 				new Identifier("dimdoors", "string_theory"),
 				new StringTheoryEnchantment(Enchantment.Rarity.UNCOMMON, EnchantmentTarget.WEARABLE, new EquipmentSlot[] {EquipmentSlot.FEET, EquipmentSlot.LEGS, EquipmentSlot.CHEST, EquipmentSlot.HEAD})
 		);
-		 */
 	}
 }
--- src/main/java/org/dimdev/dimdoors/enchantment/StringTheoryEnchantment.java
+++ src/main/java/org/dimdev/dimdoors/enchantment/StringTheoryEnchantment.java
@@ -8,16 +8,18 @@ public class StringTheoryEnchantment extends Enchantment {
 	public StringTheoryEnchantment(Rarity weight, EnchantmentTarget type, EquipmentSlot[] slotTypes) {
 		super(weight, type, slotTypes);
 	}
+
 	@Override
 	public int getMinPower(int level) {
 		return 10000;
 	}
+
 	@Override
 	public int getMaxLevel() {
 		return 1;
 	}
+
 	public boolean isTreasure() {
 		return true;
 	}
-
 }
