0c911f2582d56e2d113f402f21cd646e8ab71570
SD <shrish.deshpande@icloud.com>
2022-01-15 10:41:56 +0530

    update yarn

--- src/main/java/org/dimdev/dimdoors/particle/client/LimboAshParticle.java
+++ src/main/java/org/dimdev/dimdoors/particle/client/LimboAshParticle.java
@@ -13,25 +13,19 @@ public class LimboAshParticle extends AscendingParticle {
 
 	protected LimboAshParticle(ClientWorld world, double x, double y, double z, double velocityX, double velocityY, double velocityZ, float scaleMultiplier, SpriteProvider spriteProvider) {
 		super(world, x, y, z, 0.1F, 0.1F, 0.01F, velocityX, velocityY, velocityZ, scaleMultiplier, spriteProvider, 0.0F, 20, 0.0125F, false);
-		this.colorRed = 0.0431F;
-		this.colorGreen = 0.0353F;
-		this.colorBlue = 0.0352F;
+		this.red = 0.0431F;
+		this.green = 0.0353F;
+		this.blue = 0.0352F;
 		this.gravityStrength = -gravityStrength;
 	}
 
 	@Environment(EnvType.CLIENT)
-	public static class Factory implements ParticleFactory<DefaultParticleType> {
-		private final SpriteProvider spriteProvider;
-
-		public Factory(SpriteProvider spriteProvider) {
-			this.spriteProvider = spriteProvider;
-		}
-
+	public record Factory(SpriteProvider spriteProvider) implements ParticleFactory<DefaultParticleType> {
 		public Particle createParticle(DefaultParticleType defaultParticleType, ClientWorld clientWorld, double d, double e, double f, double g, double h, double i) {
 			Random random = clientWorld.random;
-			double j = (double)random.nextFloat() * 0.4D * (double)random.nextFloat() * 0.1D;
-			double k = (double)random.nextFloat() * 0.8D * (double)random.nextFloat() * 0.1D;// * 5.0D;
-			double l = (double)random.nextFloat() * 0.4D * (double)random.nextFloat() * 0.1D;
+			double j = (double) random.nextFloat() * 0.4D * (double) random.nextFloat() * 0.1D;
+			double k = (double) random.nextFloat() * 0.8D * (double) random.nextFloat() * 0.1D;// * 5.0D;
+			double l = (double) random.nextFloat() * 0.4D * (double) random.nextFloat() * 0.1D;
 			return new LimboAshParticle(clientWorld, d, e, f, j, k, l, 1.0F, this.spriteProvider);
 		}
 	}
