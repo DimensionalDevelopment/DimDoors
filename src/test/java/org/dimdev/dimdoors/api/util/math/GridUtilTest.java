3da8800bc206697341fc6345dbdec039d0b5dced
Waterpicker <waterpickerenternity@gmail.com>
2022-03-15 00:10:29 -0500

    Fixed day time in limbo. ALso finished disabling testing.

--- src/test/java/org/dimdev/dimdoors/api/util/math/GridUtilTest.java
+++ src/test/java/org/dimdev/dimdoors/api/util/math/GridUtilTest.java
@@ -1,27 +1,27 @@
-package org.dimdev.dimdoors.api.util.math;
-
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
-public class GridUtilTest {
-
-	@Test
-	public void idToGridPos() {
-		GridUtil.GridPos pos;
-		// I scribbled the grid down so I could figure out these tests, lmao
-		pos = GridUtil.idToGridPos(86);
-		assertEquals(new GridUtil.GridPos(8, 1), pos);
-		pos = GridUtil.idToGridPos(90);
-		assertEquals(new GridUtil.GridPos(12, 0), pos);
-		pos = GridUtil.idToGridPos(100);
-		assertEquals(new GridUtil.GridPos(7, 0), pos);
-	}
-
-	@Test
-	public void conversionConsistencyTest() {
-		for (int i = 0; i < 1000; i++) {
-			assertEquals(i, GridUtil.gridPosToID(GridUtil.idToGridPos(i)));
-		}
-	}
-}
+//package org.dimdev.dimdoors.api.util.math;
+//
+//import org.junit.Test;
+//
+//import static org.junit.Assert.*;
+//
+//public class GridUtilTest {
+//
+//	@Test
+//	public void idToGridPos() {
+//		GridUtil.GridPos pos;
+//		// I scribbled the grid down so I could figure out these tests, lmao
+//		pos = GridUtil.idToGridPos(86);
+//		assertEquals(new GridUtil.GridPos(8, 1), pos);
+//		pos = GridUtil.idToGridPos(90);
+//		assertEquals(new GridUtil.GridPos(12, 0), pos);
+//		pos = GridUtil.idToGridPos(100);
+//		assertEquals(new GridUtil.GridPos(7, 0), pos);
+//	}
+//
+//	@Test
+//	public void conversionConsistencyTest() {
+//		for (int i = 0; i < 1000; i++) {
+//			assertEquals(i, GridUtil.gridPosToID(GridUtil.idToGridPos(i)));
+//		}
+//	}
+//}
--- src/test/java/org/dimdev/test/ClientTestRunner.java
+++ src/test/java/org/dimdev/test/ClientTestRunner.java
@@ -1,16 +1,16 @@
-package org.dimdev.test;
-
-import net.devtech.potatounit.TestRunner;
-import net.fabricmc.api.EnvType;
-import org.junit.runners.model.InitializationError;
-
-public class ClientTestRunner extends TestRunner {
-	public ClientTestRunner(Class<?> testClass) throws InitializationError, ReflectiveOperationException {
-		super(hackyOrderMethod(testClass), EnvType.CLIENT);
-	}
-
-	private static Class<?> hackyOrderMethod(Class<?> testClass) {
-		System.setProperty("fabric.dli.main", "net.fabricmc.loader.launch.knot.KnotClient");
-		return testClass;
-	}
-}
+//package org.dimdev.test;
+//
+//import net.devtech.potatounit.TestRunner;
+//import net.fabricmc.api.EnvType;
+//import org.junit.runners.model.InitializationError;
+//
+//public class ClientTestRunner extends TestRunner {
+//	public ClientTestRunner(Class<?> testClass) throws InitializationError, ReflectiveOperationException {
+//		super(hackyOrderMethod(testClass), EnvType.CLIENT);
+//	}
+//
+//	private static Class<?> hackyOrderMethod(Class<?> testClass) {
+//		System.setProperty("fabric.dli.main", "net.fabricmc.loader.launch.knot.KnotClient");
+//		return testClass;
+//	}
+//}
