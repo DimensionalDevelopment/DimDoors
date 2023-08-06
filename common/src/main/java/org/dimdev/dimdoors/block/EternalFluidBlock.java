51f63ca20ad18b29af88e4db06b0f057a74ec2c2
Waterpicker <waterpickerenternity@gmail.com>
2023-08-06 09:41:30 -0500

    Fluids

--- common/src/main/java/org/dimdev/dimdoors/block/EternalFluidBlock.java
+++ common/src/main/java/org/dimdev/dimdoors/block/EternalFluidBlock.java
@@ -12,28 +12,28 @@ import org.dimdev.dimdoors.api.util.math.MathUtil;
 import org.dimdev.dimdoors.entity.limbo.LimboExitReason;
 import org.dimdev.dimdoors.fluid.ModFluids;
 import org.dimdev.dimdoors.rift.targets.EscapeTarget;
-public class EternalFluidBlock extends ArchitecturyLiquidBlock {
-	private static final EntityTarget TARGET = new EscapeTarget(true);
-
-	public EternalFluidBlock(Properties settings) {
- 		super(ModFluids.ETERNAL_FLUID, settings);
-	}
-
-
-	@Override
-	public void entityInside(BlockState blockState, Level level, BlockPos blockPos, Entity entity) {
-		if (level.isClientSide()) {
-			return;
-		}
-
-		try {
-			if (TARGET.receiveEntity(entity, Vec3.ZERO, MathUtil.entityEulerAngle(entity), entity.getDeltaMovement())) {
-				if (entity instanceof Player) {
-					LimboExitReason.ETERNAL_FLUID.broadcast((Player) entity);
-				}
-			}
-		} catch (Throwable e) {
-			e.printStackTrace();
-		}
-	}
-}
+//public class EternalFluidBlock extends ArchitecturyLiquidBlock {
+//	private static final EntityTarget TARGET = new EscapeTarget(true);
+//
+//	public EternalFluidBlock(Properties settings) {
+// 		super(ModFluids.ETERNAL_FLUID, settings);
+//	}
+//
+//
+//	@Override
+//	public void entityInside(BlockState blockState, Level level, BlockPos blockPos, Entity entity) {
+//		if (level.isClientSide()) {
+//			return;
+//		}
+//
+//		try {
+//			if (TARGET.receiveEntity(entity, Vec3.ZERO, MathUtil.entityEulerAngle(entity), entity.getDeltaMovement())) {
+//				if (entity instanceof Player) {
+//					LimboExitReason.ETERNAL_FLUID.broadcast((Player) entity);
+//				}
+//			}
+//		} catch (Throwable e) {
+//			e.printStackTrace();
+//		}
+//	}
+//}
--- common/src/main/java/org/dimdev/dimdoors/fluid/EternalFluid.java
+++ common/src/main/java/org/dimdev/dimdoors/fluid/EternalFluid.java
@@ -1,5 +1,7 @@
 package org.dimdev.dimdoors.fluid;
 
+import dev.architectury.core.fluid.ArchitecturyFlowingFluid;
+import dev.architectury.core.fluid.ArchitecturyFluidAttributes;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.core.BlockPos;
@@ -22,146 +24,60 @@ import org.dimdev.dimdoors.block.ModBlocks;
 import org.dimdev.dimdoors.item.ModItems;
 
 public abstract class EternalFluid extends FlowingFluid {
-	@Override
-	public Fluid getFlowing() {
-		return ModFluids.FLOWING_ETERNAL_FLUID.get();
-	}
 
-	@Override
-	public FluidState getSource(boolean bl) {
-		return ModFluids.ETERNAL_FLUID.get().defaultFluidState();
-	}
 
-	@Override
-	public Item getBucket() {
-		return ModItems.ETERNAL_FLUID_BUCKET.get();
-	}
+	public static interface EternalFluidDetails {
+	//Current Methods - TODO: Upgrade architectury's api to make not needed anymore.
+		default void randomTick(Level level, BlockPos blockPos, FluidState fluidState, RandomSource randomSource) {}
 
-	@Override
-	protected void randomTick(Level level, BlockPos blockPos, FluidState fluidState, RandomSource randomSource) {
+		@Environment(EnvType.CLIENT)
+		default void animateTick(Level level, BlockPos blockPos, FluidState fluidState, RandomSource randomSource) {}
 
-	}
+		default void beforeDestroyingBlock(LevelAccessor levelAccessor, BlockPos blockPos, BlockState blockState) {}
 
-	@Override
-	@Environment(EnvType.CLIENT)
-	protected void animateTick(Level level, BlockPos blockPos, FluidState fluidState, RandomSource randomSource) {
-
-	}
-
-	@Override
-	protected void beforeDestroyingBlock(LevelAccessor levelAccessor, BlockPos blockPos, BlockState blockState) {
-
-	}
-
-	@Override
-	protected int getDropOff(LevelReader levelReader) {
-		return levelReader.dimensionType().ultraWarm() ? 4 : 2;
-	}
-
-	@Override
-	protected BlockState createLegacyBlock(FluidState fluidState) {
-		return ModBlocks.ETERNAL_FLUID.get().defaultBlockState().setValue(LEVEL, getLegacyLevel(fluidState));
-	}
-
-	@Override
-	public boolean isSame(Fluid fluid) {
-		return fluid == ModFluids.ETERNAL_FLUID || fluid == ModFluids.FLOWING_ETERNAL_FLUID;
-	}
-
-	@Override
-	protected int getSlopeFindDistance(LevelReader levelReader) {
-		return levelReader.dimensionType().ultraWarm() ? 1 : 2;
-	}
-
-	@Override
-	protected boolean canBeReplacedWith(FluidState fluidState, BlockGetter blockGetter, BlockPos blockPos, Fluid fluid, Direction direction) {
-		return fluidState.getHeight(blockGetter, blockPos) >= 0.44444445F && fluid.isSame(Fluids.WATER);
-	}
-
-	@Override
-	public int getTickDelay(LevelReader levelReader) {
-		return levelReader.dimensionType().ultraWarm() ? 10 : 30;
-	}
-
-	@Override
-	protected int getSpreadDelay(Level level, BlockPos blockPos, FluidState fluidState, FluidState fluidState2) {
-		int tickDelay = this.getTickDelay(level);
-
-		if (!fluidState.isEmpty() && !fluidState2.isEmpty() && !fluidState.getValue(FALLING) && !fluidState2.getValue(FALLING) && fluidState2.getHeight(level, blockPos) > fluidState.getHeight(level, blockPos) && level.getRandom().nextInt(4) != 0) {
-			tickDelay *= 4;
+		//TODO: Submit PR to architectury to use LevelReader
+		default int getDropOff(LevelReader levelReader) {
+			return levelReader.dimensionType().ultraWarm() ? 4 : 2;
 		}
 
-		return tickDelay;
-	}
+		//TODO: Submit PR to architectury to use LevelReader
+		default int getSlopeFindDistance(LevelReader levelReader) {
+			return levelReader.dimensionType().ultraWarm() ? 1 : 2;
+		}
 
-	@Override
-	protected boolean canConvertToSource(Level level) {
-		return false;
-	}
+		//TODO: Submit PR to architectury to use LevelReader
+		default int getTickDelay(LevelReader levelReader) {
+			return levelReader.dimensionType().ultraWarm() ? 10 : 30;
+		}
 
-	@Override
-	protected void spreadTo(LevelAccessor levelAccessor, BlockPos blockPos, BlockState blockState, Direction direction, FluidState fluidState) {
-		if (direction == Direction.DOWN) {
-			if (levelAccessor.getFluidState(blockPos).is(FluidTags.WATER)) {
-				if (blockState.getBlock() instanceof LiquidBlock) {
-					levelAccessor.setBlock(blockPos, ModBlocks.BLACK_ANCIENT_FABRIC.get().defaultBlockState(), 3);
-				}
+		default int getSpreadDelay(Level level, BlockPos blockPos, FluidState fluidState, FluidState fluidState2) {
+			int tickDelay = this.getTickDelay(level);
 
-				return;
+			if (!fluidState.isEmpty() && !fluidState2.isEmpty() && !fluidState.getValue(FALLING) && !fluidState2.getValue(FALLING) && fluidState2.getHeight(level, blockPos) > fluidState.getHeight(level, blockPos) && level.getRandom().nextInt(4) != 0) {
+				tickDelay *= 4;
 			}
+
+			return tickDelay;
 		}
 
-		super.spreadTo(levelAccessor, blockPos, blockState, direction, fluidState);
-	}
+		default void spreadTo(LevelAccessor levelAccessor, BlockPos blockPos, BlockState blockState, Direction direction, FluidState fluidState) {
+			if (direction == Direction.DOWN) {
+				if (levelAccessor.getFluidState(blockPos).is(FluidTags.WATER)) {
+					if (blockState.getBlock() instanceof LiquidBlock) {
+						levelAccessor.setBlock(blockPos, ModBlocks.BLACK_ANCIENT_FABRIC.get().defaultBlockState(), 3);
+					}
 
-	@Override
-	protected boolean isRandomlyTicking() {
-		return true;
-	}
+					return;
+				}
+			}
 
-
-	@Override
-	protected float getExplosionResistance() {
-		return 100000;
-	}
-
-	public static class Flowing extends EternalFluid {
-
-		@Override
-		protected void createFluidStateDefinition(StateDefinition.Builder<Fluid, FluidState> builder) {
-			builder.add(LEVEL);
+			self().spreadTo(levelAccessor, blockPos, blockState, direction, fluidState);
 		}
 
-		@Override
-		public Fluid getSource() {
-			return null;
-		}
+		EternalFluidDetails self();
 
-		@Override
-		public boolean isSource(FluidState fluidState) {
-			return false;
-		}
-
-		@Override
-		public int getAmount(FluidState fluidState) {
-			return fluidState.getValue(LEVEL);
-		}
-	}
-
-	public static class Still extends EternalFluid {
-		@Override
-		public Fluid getSource() {
-			return ModFluids.ETERNAL_FLUID.get().defaultFluidState().getType();
-		}
-
-		@Override
-		public boolean isSource(FluidState fluidState) {
-			return false;
-		}
-
-		@Override
-		public int getAmount(FluidState fluidState) {
-			return 8;
+		default boolean isRandomlyTicking() {
+			return true;
 		}
 	}
 }
