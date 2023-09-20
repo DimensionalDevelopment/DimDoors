3071a9d7ab11bdf30cb030646a69f4f025ad297c
TheComputerizer <dcpixelation42@gmail.com>
2023-09-20 16:18:29 -0500

    Update build script and version

--- src/main/java/org/dimdev/dimdoors/DimDoors.java
+++ src/main/java/org/dimdev/dimdoors/DimDoors.java
@@ -48,7 +48,7 @@ public class DimDoors {
     public static final String MODID = "dimdoors";
     public static final String MODNAME = "Dimensional Doors";
     public static final String MCVERSIONS = "[1.12,1.13)";
-    public static final String VERSION = "3.1.0";
+    public static final String VERSION = "3.2.0";
     public static final String DEPENDENCIES = "required-after:forge@[14.23.5.2860,)";
 
     @Mod.Instance(DimDoors.MODID)
