0af38678a8c1fb6f4dab22f1751f56273a80c374
Waterpicker <waterpickerenternity@gmail.com>
2026-02-05 22:59:09 -0600

    Fix cloth config gui issues.

--- common/src/main/java/org/dimdev/dimdoors/client/DimensionalDoorsClient.java
+++ common/src/main/java/org/dimdev/dimdoors/client/DimensionalDoorsClient.java
@@ -8,7 +8,11 @@ import dev.architectury.platform.Platform;
 import dev.architectury.registry.client.rendering.BlockEntityRendererRegistry;
 import dev.architectury.registry.menu.MenuRegistry;
 import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.gui.registry.GuiRegistry;
+import me.shedaniel.autoconfig.gui.registry.api.GuiProvider;
+import me.shedaniel.autoconfig.gui.registry.api.GuiRegistryAccess;
 import me.shedaniel.autoconfig.util.Utils;
+import me.shedaniel.clothconfig2.api.AbstractConfigListEntry;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -18,11 +22,15 @@ import net.minecraft.client.particle.SpriteSet;
 import net.minecraft.client.renderer.ShaderInstance;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.core.Registry;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleType;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
 import org.dimdev.dimdoors.DimensionalDoors;
@@ -70,18 +78,15 @@ public class DimensionalDoorsClient {
 		Platform.getMod(DimensionalDoors.MOD_ID).registerConfigurationScreen(ModMenu::getConfigScreen);
 		ClientPlayerEvent.CLIENT_PLAYER_JOIN.register((handler) -> ClientPacketHandler.sendPacket(new NetworkHandlerInitializedC2SPacket()));
 
-		ClientGuiEvent.DEBUG_TEXT_LEFT.register(new ClientGuiEvent.DebugText() {
-			@Override
-			public void gatherText(List<String> strings) {
-				assert Minecraft.getInstance().player != null;
-				HitResult hit = RaycastHelper.findDetachRift(Minecraft.getInstance().player, RaycastHelper.DETACH);
-				if(hit.getType() == HitResult.Type.BLOCK) {
-					if(Minecraft.getInstance().level.getBlockEntity(((BlockHitResult) hit).getBlockPos()) instanceof DetachedRiftBlockEntity rift) {
-						strings.add("Size: " + rift.size);
-					}
-				}
-			}
-		});
+		ClientGuiEvent.DEBUG_TEXT_LEFT.register(strings -> {
+            assert Minecraft.getInstance().player != null;
+            HitResult hit = RaycastHelper.findDetachRift(Minecraft.getInstance().player, RaycastHelper.DETACH);
+            if(hit.getType() == HitResult.Type.BLOCK) {
+                if(Minecraft.getInstance().level.getBlockEntity(((BlockHitResult) hit).getBlockPos()) instanceof DetachedRiftBlockEntity rift) {
+                    strings.add("Size: " + rift.size);
+                }
+            }
+        });
 
 		registerCompats();
 
@@ -90,11 +95,25 @@ public class DimensionalDoorsClient {
 		initBlockEntitiesClient();
 		ModBlocks.initClient();
 
-		AutoConfig.getGuiRegistry(ModConfig.class).registerPredicateProvider((i18n, field, config, defaults, registry) -> Collections.singletonList(ENTRY_BUILDER
-				.startStrList(Component.translatable(i18n), ((Set<String>) Utils.getUnsafely(field, config, defaults)).stream().toList())
-				.setSaveConsumer(newValue -> Utils.setUnsafely(field, config, newValue.stream().collect(Collectors.toSet()))).build()), isSetOfType(String.class));
+        var guiRegistry = AutoConfig.getGuiRegistry(ModConfig.class);
 
-//		DimensionRenderering.initClient();
+        guiRegistry.registerPredicateProvider(new GuiProvider() {
+            @Override
+            public List<AbstractConfigListEntry> get(String i18n, Field field, Object config, Object defaults, GuiRegistryAccess registry) {
+                return Collections.singletonList(ENTRY_BUILDER
+                        .startStrList(Component.translatable(i18n), ((Set<String>) Utils.getUnsafely(field, config, defaults)).stream().toList())
+                        .setSaveConsumer(newValue -> Utils.setUnsafely(field, config, newValue.stream().collect(Collectors.toSet()))).build());
+            }
+        }, isSetOfType(String.class));
+
+        guiRegistry.registerPredicateProvider((i18n, field, config, defaults, registry) -> Collections.singletonList(ENTRY_BUILDER
+                .startStrList(Component.translatable(i18n), ((List<ResourceKey<?>>) Utils.getUnsafely(field, config, defaults)).stream().map(ResourceKey::location).map(ResourceLocation::toString).toList())
+                .setSaveConsumer(newValue -> Utils.setUnsafely(field, config, newValue.stream().map(ResourceLocation::parse).map(a -> ResourceKey.create(Registries.DIMENSION, a)).toList())).build()),
+                isResourceKeyListOfType(Level.class));
+
+        guiRegistry.registerPredicateProvider((i18n, field, config, defaults, registry) -> Collections.singletonList(ENTRY_BUILDER
+                        .startStrField(Component.translatable(i18n), ((ResourceKey<?>) Utils.getUnsafely(field, config, defaults)).location().toString())
+                .setSaveConsumer(newValue -> Utils.setUnsafely(field, config, ResourceKey.create(Registries.DIMENSION, ResourceLocation.parse(newValue)))).build()), isResourceKeyOfType(Level.class));
 
 		registerListeners();
 
@@ -124,18 +143,34 @@ public class DimensionalDoorsClient {
 		BlockEntityRendererRegistry.register(ModBlockEntityTypes.DETACHED_RIFT.get(), ctx -> new DetachedRiftBlockEntityRenderer());
 	}
 
-	private static Predicate<Field> isListOfType(Type... types) {
-		return field -> {
-			if (List.class.isAssignableFrom(field.getType()) && field.getGenericType() instanceof ParameterizedType) {
-				Type[] args = ((ParameterizedType) field.getGenericType()).getActualTypeArguments();
-				return args.length == 1 && Stream.of(types).anyMatch(type -> Objects.equals(args[0], type));
-			} else {
-				return false;
-			}
-		};
-	}
+    private static Predicate<Field> isResourceKeyListOfType(Class<?> registryType) {
+        return field -> {
+            if (List.class.isAssignableFrom(field.getType()) && field.getGenericType() instanceof ParameterizedType) {
+                Type[] args = ((ParameterizedType) field.getGenericType()).getActualTypeArguments();
+                if (args.length == 1 && args[0] instanceof ParameterizedType paramType) {
+                    if (paramType.getRawType().equals(ResourceKey.class)) {
+                        Type[] resourceKeyArgs = paramType.getActualTypeArguments();
+                        return resourceKeyArgs.length == 1 && Objects.equals(resourceKeyArgs[0], registryType);
+                    }
+                }
+            }
+            return false;
+        };
+    }
 
-	private static Predicate<Field> isSetOfType(Type... types) {
+    private static Predicate<Field> isResourceKeyOfType(Class<?> registryType) {
+        return field -> {
+            if (field.getGenericType() instanceof ParameterizedType paramType) {
+                if (paramType.getRawType().equals(ResourceKey.class)) {
+                    Type[] resourceKeyArgs = paramType.getActualTypeArguments();
+                    return resourceKeyArgs.length == 1 && Objects.equals(resourceKeyArgs[0], registryType);
+                }
+            }
+            return false;
+        };
+    }
+
+    private static Predicate<Field> isSetOfType(Type... types) {
 		return field -> {
 			if (Set.class.isAssignableFrom(field.getType()) && field.getGenericType() instanceof ParameterizedType) {
 				Type[] args = ((ParameterizedType) field.getGenericType()).getActualTypeArguments();
